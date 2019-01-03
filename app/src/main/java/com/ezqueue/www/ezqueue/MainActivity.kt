ab65f63b2163a4fbf3e8373b2d1537dcf178006d
yochida <yochida2015@gmail.com>
2019-01-03 10:21:08 +0700

    commit Toast

--- app/src/main/java/com/ezqueue/www/ezqueue/MainActivity.kt
+++ app/src/main/java/com/ezqueue/www/ezqueue/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ezqueue.www.ezqueue
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_add.setOnClickListener({
+            Toast.makeText(this, "Added", Toast.LENGTH_LONG).show()
+            
+        })
     }
 }
